ba0e60eba6196d295509eb7439396559841c44d1
andyvuong <andyv.42@gmail.com>
2016-11-04 17:20:45 -0500

    Add basic frontend views

---
+++ app/frontend/public/js/controllers.js
@@ -0,0 +1,33 @@
+var mp4Controllers = angular.module('passportControllers', []);
+
+mp4Controllers.controller('LoginController', ['$scope', 'CommonData'  , function($scope, CommonData) {
+  $scope.data = "";
+   $scope.displayText = ""
+
+  $scope.setData = function(){
+    CommonData.setData($scope.data);
+    $scope.displayText = "Data set"
+
+  };
+
+}]);
+
+mp4Controllers.controller('SignupController', ['$scope', 'CommonData' , function($scope, CommonData) {
+  $scope.data = "";
+
+  $scope.getData = function(){
+    $scope.data = CommonData.getData();
+
+  };
+
+}]);
+
+
+mp4Controllers.controller('profileController', ['$scope', '$http', 'Llamas', '$window' , function($scope, $http,  Llamas, $window) {
+
+  Llamas.get().success(function(data){
+    $scope.llamas = data;
+  });
+
+
+}]);
